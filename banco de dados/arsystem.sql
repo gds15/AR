7710b55a4a3c97166d38beb04fbc2d8acc2bd5b3
gds15 <gugadellatorre@gmail.com>
2019-01-18 08:54:34 -0200

    banco novo

--- banco de dados/arsystem.sql
+++ banco de dados/arsystem.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 27-Nov-2018 às 20:03
+-- Generation Time: 18-Jan-2019 às 11:52
 -- Versão do servidor: 10.1.36-MariaDB
 -- versão do PHP: 7.2.11
 
@@ -34,23 +34,23 @@ CREATE TABLE `conta` (
   `descricao` varchar(100) DEFAULT NULL,
   `valor` double DEFAULT NULL,
   `valorPago` double DEFAULT NULL,
-  `multaJuros` double DEFAULT NULL,
-  `mes` varchar(2) DEFAULT NULL
+  `dataPagamento` date DEFAULT NULL,
+  `ativo` varchar(1) DEFAULT NULL,
+  `motivoEstorno` varchar(50) DEFAULT NULL,
+  `quemEstornou` varchar(45) DEFAULT NULL,
+  `tipo` varchar(1) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Extraindo dados da tabela `conta`
 --
 
-INSERT INTO `conta` (`id`, `data`, `descricao`, `valor`, `valorPago`, `multaJuros`, `mes`) VALUES
-(2, '2018-11-29', 'agua', 120, 120, 0, '11'),
-(4, '2018-11-19', 'teste', 120, 120, 0, '11'),
-(5, '2018-11-19', 'luz', 60, 60, 0, '11'),
-(6, '2018-11-19', 'agua', 60, 60, 0, '11'),
-(7, '2018-11-19', 'aluguel', 1000, 1000, 0, '11'),
-(8, '2018-11-19', 'telefone', 500, 500, 0, '11'),
-(9, '2018-11-27', 'outros', 1, 1000, 0, '11'),
-(10, '2018-11-27', 'encomenda', 500, 0, 0, '11');
+INSERT INTO `conta` (`id`, `data`, `descricao`, `valor`, `valorPago`, `dataPagamento`, `ativo`, `motivoEstorno`, `quemEstornou`, `tipo`) VALUES
+(18, '2018-12-06', 'luz', 150, 0, '0000-00-00', 'n', NULL, NULL, NULL),
+(19, '2018-12-06', 'agua', 1, 1, '2018-12-06', 's', NULL, NULL, NULL),
+(20, '2018-12-14', 'aluguel', 20, 20, '2018-12-14', 's', NULL, NULL, NULL),
+(21, '2018-12-19', 'transporte', 10, 10, '2018-12-15', 'n', 'conta paga 2 x', 'gds', 'e'),
+(22, '2018-12-19', 'outros', 1000, 1100, '2018-12-20', 's', NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -101,21 +101,8 @@ CREATE TABLE `dizimo` (
 --
 
 INSERT INTO `dizimo` (`id`, `membro`, `valor`, `desc`, `data`, `usuariocds`, `mes`) VALUES
-(2, 4, 5000, 'teste', '2018-11-27', 4, '11');
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `foto`
---
-
-CREATE TABLE `foto` (
-  `id` int(11) NOT NULL,
-  `album` varchar(100) NOT NULL,
-  `data` int(11) NOT NULL,
-  `imagem` varchar(20) NOT NULL,
-  `ativo` enum('Sim','Não') NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+(2, 4, 5000, 'teste', '2018-11-27', 4, '11'),
+(3, 3, 5000, 'nada', '2018-12-17', 4, '12');
 
 -- --------------------------------------------------------
 
@@ -164,7 +151,7 @@ CREATE TABLE `membro` (
   `telefone` varchar(17) NOT NULL,
   `endereco` varchar(100) NOT NULL,
   `cidade` varchar(100) NOT NULL,
-  `estado` enum('Acre','Alagoas','Amapá','Amazonas','Bahia','Ceará','Distrito Federal','Espírito Santo','Goiás','Maranhão','Mato Grosso','Mato Grosso do Sul','Minas Gerais','Pará','Paraíba','Paraná','Pernambuco','Piauí','Rio de Janeiro','Rio Grande do Norte','Rio Grande do Sul','Rondônia','Roraima','Santa Catarina','São Paulo','Sergipe','Tocantins') NOT NULL,
+  `estado` varchar(30) NOT NULL,
   `bairro` varchar(100) NOT NULL,
   `cep` char(9) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -175,26 +162,8 @@ CREATE TABLE `membro` (
 
 INSERT INTO `membro` (`id`, `nome`, `funcao_id`, `email`, `cpf`, `datanascimento`, `telefone`, `endereco`, `cidade`, `estado`, `bairro`, `cep`) VALUES
 (3, 'robinho', 2, 'robinho2708@hotmail.com', '555.555.555-55', '1990-08-10', '(44) 4444-44444', 'avenida estação, 1801', 'umuarama', '', 'centro', '87505-090'),
-(4, 'gustavo', 8, 'gugadellatorre@gmail.com', '561.561.561-51', '0000-00-00', '(65) 4156-1561_', 'dsfdfdfdfdf', 'Perobal', '', 'fbfbfb', '56565-656');
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `oferta`
---
-
-CREATE TABLE `oferta` (
-  `id` int(11) NOT NULL,
-  `titulo` varchar(100) NOT NULL,
-  `ativo` enum('Sim','Não') NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Extraindo dados da tabela `oferta`
---
-
-INSERT INTO `oferta` (`id`, `titulo`, `ativo`) VALUES
-(10, 'dizimo', 'Sim');
+(4, 'gustavo', 8, 'gugadellatorre@gmail.com', '561.561.561-51', '0000-00-00', '(65) 4156-1561_', 'dsfdfdfdfdf', 'Perobal', '', 'fbfbfb', '56565-656'),
+(5, 'GUSTAVO DELLATORRE DA SILVA', 13, 'gugadellatorre@gmail.com', '622.222.222-22', '0000-00-00', '(65) 4156-1561_', 'dsfdfdfdfdf', 'Perobal', 'PR', 'fdfdfdfd', '87539-000');
 
 -- --------------------------------------------------------
 
@@ -252,21 +221,6 @@ INSERT INTO `usuario` (`id`, `nome`, `email`, `login`, `senha`, `ativo`, `nivel`
 (27, 'gustavo dellatorre', 'gugadellatorre@gmail.com', 'guga', '47bce5c74f589f4867dbd57e9ca9f808', 'Sim', 1),
 (28, 'zezinho', 'zezinho@gmail.com', 'ze', '202cb962ac59075b964b07152d234b70', 'Sim', 2);
 
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `video`
---
-
-CREATE TABLE `video` (
-  `id` int(11) NOT NULL,
-  `titulo` varchar(100) NOT NULL,
-  `original` varchar(100) NOT NULL,
-  `data` int(11) NOT NULL,
-  `imagem` varchar(20) NOT NULL,
-  `ativo` enum('Sim','Não') NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
 --
 -- Indexes for dumped tables
 --
@@ -294,12 +248,6 @@ ALTER TABLE `dizimo`
   ADD KEY `fkk2_idx` (`usuariocds`);
 
 --
--- Indexes for table `foto`
---
-ALTER TABLE `foto`
-  ADD PRIMARY KEY (`id`);
-
---
 -- Indexes for table `funcao`
 --
 ALTER TABLE `funcao`
@@ -315,12 +263,6 @@ ALTER TABLE `membro`
   ADD KEY `fk_membro_funcao_idx` (`funcao_id`,`nome`);
 
 --
--- Indexes for table `oferta`
---
-ALTER TABLE `oferta`
-  ADD PRIMARY KEY (`id`);
-
---
 -- Indexes for table `tipoevento`
 --
 ALTER TABLE `tipoevento`
@@ -333,12 +275,6 @@ ALTER TABLE `usuario`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `video`
---
-ALTER TABLE `video`
-  ADD PRIMARY KEY (`id`);
-
---
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -346,7 +282,7 @@ ALTER TABLE `video`
 -- AUTO_INCREMENT for table `conta`
 --
 ALTER TABLE `conta`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT for table `culto`
@@ -358,13 +294,7 @@ ALTER TABLE `culto`
 -- AUTO_INCREMENT for table `dizimo`
 --
 ALTER TABLE `dizimo`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT for table `foto`
---
-ALTER TABLE `foto`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `funcao`
@@ -376,13 +306,7 @@ ALTER TABLE `funcao`
 -- AUTO_INCREMENT for table `membro`
 --
 ALTER TABLE `membro`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- AUTO_INCREMENT for table `oferta`
---
-ALTER TABLE `oferta`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `tipoevento`
@@ -395,12 +319,6 @@ ALTER TABLE `tipoevento`
 --
 ALTER TABLE `usuario`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
-
---
--- AUTO_INCREMENT for table `video`
---
-ALTER TABLE `video`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Constraints for dumped tables
